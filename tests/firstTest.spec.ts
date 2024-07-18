7801644e564d09ac212019fa293f716db9022b74
John D <johnd@Johns-MBP.attlocal.net>
2024-07-18 10:14:41 -0500

    test update again

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,5 +1,6 @@
-import {test} from '@playwright/test'
+import {test, expect} from '@playwright/test'
 
-test('the first test',async ({page}) => {
-     await page.goto('http://localhost:4200/')
+
+test('the first test', async ({page}) => {
+     await page.goto('http://localhost:4200/');
 })
