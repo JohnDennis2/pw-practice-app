4912f6dc0c63bf5d7cecf66bac3629b64ef9deab
John D <johnd@Johns-MBP.attlocal.net>
2024-08-27 16:45:35 -0500

    update

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -9,10 +9,10 @@ test('the first test', async ({page}) => {
      await page.goto('http://localhost:4200/')
      await page.getByText('Forms').click()
      await page.getByText('Form Layouts').click()
-})
+});
 
 test('user facing locators', async ({page}) => {
      await page.goto('http://localhost:4200/')
 await page.getByRole ('textbox', {name: "Email"}).first().click()
 await page.getByRole('button', {name: "Sign in"}).first().click()
-})
+});
