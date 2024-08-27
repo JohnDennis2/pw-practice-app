a3f4552169d92f72860bb17176cc9979abcf22cd
John D <johnd@Johns-MBP.attlocal.net>
2024-08-27 14:35:19 -0500

    new forms

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,5 +2,7 @@ import {test, expect} from '@playwright/test'
 
 
 test('the first test', async ({page}) => {
-     await page.goto('http://localhost:4200/');
+     await page.goto('http://localhost:4200/')
+     await page.getByText('Forms').click()
+     await page.getByText('Form Layouts').click()
 })
