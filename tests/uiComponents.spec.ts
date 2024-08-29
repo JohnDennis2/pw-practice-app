740365ae3b44146f39ff8f1c0522ef289a8b67ec
John D <johnd@Johns-MBP.attlocal.net>
2024-08-29 17:02:01 -0500

    new assertions and test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -12,9 +12,17 @@ test.describe('Form Layouts page', () => {
     test('input fields', async({page}) => {
         const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name:"Email"})
         
-        await usingTheGridEmailInput.fill('test@test.com').clear()
+        await usingTheGridEmailInput.fill('test@test.com')
         await usingTheGridEmailInput.clear()
+    await usingTheGridEmailInput.pressSequentially('test2@test.com')
     
-    
+
+    // generic assertions
+    const inputValue = await usingTheGridEmailInput.inputValue()
+    expect(inputValue).toEqual('test2@test.com')
+
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveText('test2@test.com')
+
     })
 })
