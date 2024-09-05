e3ef91d1cacfff556e152c5b80cf389eb94845fb
John D <johnd@Johns-MBP.attlocal.net>
2024-09-05 13:14:56 -0500

    dropdown menus contind

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -78,6 +78,23 @@ test('list and drpdowns', async ({page}) => {
 
         const optionList = page.locator('nb-options-list nb-options')
         await expect(optionList).toHaveText(["Light"," Dark", "Cosmic" , "Coroprate"])
+        await optionList.filter({hasText: "Cosmic"}).click()
+
+        const header = page.locator('nb-layout-header')
+        await expect(header).toHaveCSS('background-color','rgb(50,50,89)')
+
+        const colors = {
+            "Light": "rgb(255,255,255)",
+            "Dark": "rgb(34, 43,, 69)",
+            "Cosmic": "rgb(50,50,89)",
+            "Corporate":"rgb(255,255,255)"
+        }
+
+        for (const color in colors){
+            await optionList.filter({hasText: color}).click()
+            await expect(header).toHaveCSS('background-color', colors[color])
+            await dropDownMenu.click()
+        }
 
 
     })
