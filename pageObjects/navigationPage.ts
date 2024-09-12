4511ff4fc249c90d54dea954116708bdd58c3b7d
John D <johnd@Johns-MBP.attlocal.net>
2024-09-12 15:17:33 -0500

    exporting nav page

--- pageObjects/navigationPage.ts
+++ pageObjects/navigationPage.ts
@@ -1,11 +1,16 @@
 import {Page} from "playwright/test";
 
-class NavigationPage {
+export class NavigationPage {
 
     readonly page: Page
 
     constructor(page : Page){
         this.page = page
 
+    }
+
+    async formLayoutsPage(){
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Forms Layouts').click()
     }
 }
