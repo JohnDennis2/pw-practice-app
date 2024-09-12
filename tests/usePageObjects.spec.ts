592d8e3c400c4c88fa70c96662bb6ac97c4b425e
John D <johnd@Johns-MBP.attlocal.net>
2024-09-12 15:21:59 -0500

    new Naviagtion Page

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,9 +1,12 @@
 import {test,expect} from '@playwright/test'
+import {NavigationPage} from  '../pageObjects/navigationPage'
 
 test.beforeEach(async({page}) => {
     await page.goto('https://localhost:4200/')
 })
 
 test('navigate to form page', async ({page})=>{
-    
+    const navigateTo= new NavigationPage(page)
+    navigateTo.formLayoutsPage()
+
 })
